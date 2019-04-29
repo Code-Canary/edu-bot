5973b3c2cd994feead8be54c2ea8eee88a75d225
Jad Salhani <salhanijad@gmail.com>
2019-04-29 13:08:58 -0700

    fix: update seeder to create questions and feed them in a lesson

--- dao/seeder/lessonSeeder.js
+++ dao/seeder/lessonSeeder.js
@@ -1,25 +1,43 @@
 let mongoose = require('mongoose');
 
 // Require models to seed
-let Lesson = require('../models/lesson');
+let Lesson = require('../models/lesson').Lesson;
+let Question = require('../models/question').Question;
+
+let questions = [
+    new Question({
+        title: "How do you write a div?",
+        type: "free_text",
+        rendered_image_path: "",
+        branches: [{
+            answer: "<div></div>",
+            question: null,
+        }]
+    }),
+    new Question({
+        title: "How can you write css colors?",
+        branches: [{
+            answer: "Hex Codes",
+            question: null,
+        },
+        {
+            answer: "With a goat>",
+            question: null,
+        },
+        {
+            answer: "Im sleepy>",
+            question: null,
+        }],
+        type: "multiple_choice",
+        rendered_image_path: ""
+    })
+];
 
 let lessons = new Lesson({
     name: "Initial Lesson",
     questions: [
-        {
-            title: "How do you write a div?",
-            answer: "<div></div>",
-            possible_answers: [],
-            type: "free_text",
-            rendered_image_path: ""
-        },
-        {
-            title: "How can you write css colors?",
-            answer: null,
-            possible_answers: ["Hex Codes", "With a goat", "Im sleepy"],
-            type: "multiple_choice",
-            rendered_image_path: ""
-        }
+        questions[0]._id,
+        questions[1]._id
     ],
     lesson_template: `
     <!DOCTYPE html>
@@ -38,20 +56,38 @@ let lessons = new Lesson({
 })
 
 console.log('Dropping existing collections');
+
 mongoose.connection.collections['lessons'].drop(function (err) {
-    console.log('lessons  collection has been dropped.');
+    console.log('lessons  ollection has been dropped.');
+    lessons.save((err) => {
+        if (err) {
+            console.log('An error has occurred while seeding lessons');
+            console.log('********************************************');
+            console.log(err);
+        } else {
+            console.log('Lessons have been successfully seeded');
+        }
+    });
+});
+mongoose.connection.collections['questions'].drop(function (err) {
+    console.log('questions collection has been dropped.');
+    console.log('Seeding Questions...');
+    questions.forEach((question) => {
+        question.save((err) => {
+            if (err) {
+                console.log('An error has occurred while seeding Questions');
+                console.log('********************************************');
+                console.log(err);
+            } else {
+                console.log('Questions have been successfully seeded');
+            }
+        });
+    });
 });
 
 mongoose.connection.collections['users'].drop(function (err) {
     console.log('users collection has been dropped.');
 });
 
-lessons.save((err) => {
-    if (err) {
-        console.log('An error has occurred while seeding lessons');
-        console.log('********************************************');
-        console.log(err);
-    } else {
-        console.log('Lessons have been successfully seeded');
-    }
-});
+
+
