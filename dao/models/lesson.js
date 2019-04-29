8040bcf4cfdc394b8bb16388af3e674ceef31860
Jad Salhani <salhanijad@gmail.com>
2019-04-29 00:07:00 -0700

    fix: model definitions

--- dao/models/lesson.js
+++ dao/models/lesson.js
@@ -1,15 +1,10 @@
 let mongoose = require('mongoose');
+let questionSchema = require('./question').questionSchema
+
 
 var lessonSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     name: String,
-    questions: [{
-        title: String,
-        answer: String, // empty for informative
-        possible_answers: [String], // used for the multi_choice options
-        type: String, // free_text, multi_choice, informative -> ignore answer value
-        rendered_image_path: String
-    }],
+    questions: [questionSchema],
     lesson_template: String
 }, { timestamps: true });
 
