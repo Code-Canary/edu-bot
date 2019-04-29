12f195306ff1a94ebdc2e84d3501a50f9212baba
Jad Salhani <salhanijad@gmail.com>
2019-04-29 00:22:42 -0700

    chore: cleanup app js

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
 'use strict';
 
 require('dotenv').config();
+
+/**
+ * DAO Section
+ * Initialize DB connection
+ * Initialize models
+ */
 const connect = require("./dao/connector");
 
 // Open DB Connection 
 connect();
 
-let Lesson = require('./dao/models/lesson');
-let User = require('./dao/models/user');
-let Question = require('./dao/models/question').Question;
-
+const Lesson = require('./dao/models/lesson');
+const User = require('./dao/models/user');
+const Question = require('./dao/models/question').Question;
 const APIController = require('./controllers/APIController');
 
-// Imports dependencies and set up http server
+// Call seeder for sample lessons.
+require("./dao/seeder/lessonSeeder")
+
+/**
+ * Express Sections
+ * Imports dependencies and set up http server
+ */
 const
   express = require('express'),
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
 
-require("./dao/seeder/lessonSeeder")
+// Webhook BOT routes
+app.get('/webhook', APIController.verifyServer);
+app.post('/webhook', APIController.handleWebhookEvent);
 
 // Handles rendering of the user homepage
 app.get('/render', function (req, res) {
   APIController.renderHtml(req, res, User);
 })
-
-// Accepts POST requests at /webhook endpoint
-app.post('/webhook', APIController.handleWebhookEvent);
-// Accepts GET requests at the /webhook endpoint
-app.get('/webhook', APIController.verifyServer);
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
