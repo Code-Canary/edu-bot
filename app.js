901e603a864146561eac791862846359c3397bf4
Pride Musvaire <pride@lazydude.co>
2019-04-29 01:09:36 -0700

    wrote placeholder function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
  * Initialize DB connection
  * Initialize models
  */
-const connect = require("./dao/connector");
+const connect = require('./dao/connector');
 
 // Open DB Connection 
 connect();
@@ -16,27 +16,32 @@ const Lesson = require('./dao/models/lesson');
 const User = require('./dao/models/user');
 const Question = require('./dao/models/question').Question;
 const APIController = require('./controllers/APIController');
+const getRawContents = require('./render/html-formatter').getRawContents;
+const getFormattedContent = require('./render/html-formatter').getFormattedContent;
+const dummy_questions = require('./render/html-formatter').dummy_questions;
 
 // Call seeder for sample lessons.
-require("./dao/seeder/lessonSeeder")
+require('./dao/seeder/lessonSeeder');
 
 /**
  * Express Sections
  * Imports dependencies and set up http server
  */
 const
-  express = require('express'),
-  body_parser = require('body-parser'),
-  app = express().use(body_parser.json()); // creates express http server
+    express = require('express'),
+    body_parser = require('body-parser'),
+    app = express().use(body_parser.json()); // creates express http server
 
 // Webhook BOT routes
 app.get('/webhook', APIController.verifyServer);
 app.post('/webhook', APIController.handleWebhookEvent);
 
+getFormattedContent(getRawContents(), dummy_questions);
+
 // Handles rendering of the user homepage
-app.get('/render', function (req, res) {
+app.get('/render', function(req, res) {
   APIController.renderHtml(req, res, User);
-})
+});
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
--- render/html-formatter.js
+++ render/html-formatter.js
@@ -1,40 +1,36 @@
-const stringUtils = require('./string-utils')
+const stringUtils = require('./string-utils');
 
-const PLACEHOLDER_DELIMITER = '$'
+const PLACEHOLDER_DELIMITER = '$';
 const DEFAULT_PLACEHOLDER_VALUE = '';
 
 exports.getRawContents = function() {
-    return `
+  return `
     <html>
-    Coming soon!!!
+    div
     </html>
     `;
-}
+};
+
+exports.dummy_questions = [
+  {
+    placeholder: 'div',
+    answer: '<div></div>',
+  },
+];
 
 /**
  * Returns HTML with all specified placeholders replaced with their values and
  * any missing placeholders is removed.
  */
 exports.getFormattedContent = function(content, placeholders) {
-    let tempContent = content;
-    while (tempContent.includes(PLACEHOLDER_DELIMITER)) {
-        const placeholderStart = tempContent.indexOf(PLACEHOLDER_DELIMITER);
-        const placeholderEnd = tempContent.indexOf(PLACEHOLDER_DELIMITER, placeholderStart + 1);
+  let tempContent = content;
 
-        if(placeholderEnd == -1) {
-            throw "A placeholder has no end delimiter";
-        }
+  placeholders.map(placeholder => {
+    tempContent = stringUtils.replaceAll(tempContent, placeholder.placeholder, placeholder.answer);
+    console.log(tempContent)
+  })
 
-        const placeholder = tempContent.substring(placeholderStart, placeholderEnd + 1);
-        const placeholderName = stringUtils.replaceAll(placeholder, PLACEHOLDER_DELIMITER, '');
+  console.log('content', tempContent);
 
-        let placeholderValue = placeholders[placeholderName];
-        if(placeholderValue == undefined) {
-            placeholderValue = DEFAULT_PLACEHOLDER_VALUE;
-        }
-
-        tempContent = stringUtils.replaceAll(tempContent, placeholder, placeholderValue);
-    }
-
-    return tempContent;
-}
+  return tempContent;
+};
