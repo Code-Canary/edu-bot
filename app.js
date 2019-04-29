cf62bd4233e14af3f607f0c1d4d24216bc12af8c
Jad Salhani <salhanijad@gmail.com>
2019-04-28 17:39:48 -0700

    chore: update to latest send api version

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ function callSendAPI(sender_psid, response) {
 
   // Send the HTTP request to the Messenger Platform
   request({
-    'uri': 'https://graph.facebook.com/v2.6/me/messages',
+    'uri': 'https://graph.facebook.com/v3.2/me/messages',
     'qs': { 'access_token': PAGE_ACCESS_TOKEN },
     'method': 'POST',
     'json': request_body,
