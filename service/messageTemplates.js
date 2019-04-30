b79f0f41be25234a58aac484c65520f3d1c54e41
Jad Salhani <salhanijad@gmail.com>
2019-04-29 20:42:09 -0700

    refactor: quick reply template

--- service/messageTemplates.js
+++ service/messageTemplates.js
@@ -1,123 +1,117 @@
 const generic = function ({ title, subtitle, buttons }) {
-  return {
-    'attachment': {
-      'type': 'template',
-      'payload': {
-        'template_type': 'generic',
-        'elements': [
-          {
-            'title': title,
-            'subtitle': subtitle,
+    return {
+        'attachment': {
+            'type': 'template',
+            'payload': {
+                'template_type': 'generic',
+                'elements': [
+                    {
+                        'title': title,
+                        'subtitle': subtitle,
+                        'buttons': [
+                            {
+                                'type': 'postback',
+                                'title': 'Yes!',
+                                'payload': 'yes',
+                            },
+                            {
+                                'type': 'postback',
+                                'title': 'No!',
+                                'payload': 'no',
+                            },
+                        ],
+                    }],
+            },
+        },
+    };
+};
+
+const quickReply = function (text, quickReplies) {
+    return {
+        'text': text,
+        'quick_replies': quickReplies,
+    };
+};
+
+const button = function ({ title, buttons }) {
+    return {
+        'payload': {
+            'template_type': 'button',
+            'text': title,
             'buttons': [
-              {
-                'type': 'postback',
-                'title': 'Yes!',
-                'payload': 'yes',
-              },
-              {
-                'type': 'postback',
-                'title': 'No!',
-                'payload': 'no',
-              },
+                buttons,
             ],
-          }],
-      },
-    },
-  };
-};
-
-const quickReply = function({ title, text, postback }) {
-  return {
-    'text': text,
-    'quick_replies':[
-      {
-        'content_type': 'text',
-        'title': title,
-        'payload': '<POSTBACK_PAYLOAD>',
-      },
-    ],
-  };
-};
-
-const button = function({ title, buttons }) {
-  return {
-    'payload': {
-      'template_type': 'button',
-      'text': title,
-      'buttons': [
-        buttons,
-      ],
-    },
-  };
+        },
+    };
 };
 
 const list = function ({ title, subtitle, buttons, image_url, url }) {
-  return {
-    'payload': {
-      'template_type': 'list',
-      'top_element_style': '<LARGE | COMPACT>',
-      'elements': [
-        {
-          'title': title,
-          'subtitle': subtitle,
-          'image_url': image_url,
-          'buttons': [buttons],
-          'default_action': {
-            'type': 'web_url',
-            'url': url,
-            'messenger_extensions': 'TRUE',
-            'webview_height_ratio': 'COMPACT',
-          },
+    return {
+        'payload': {
+            'template_type': 'list',
+            'top_element_style': '<LARGE | COMPACT>',
+            'elements': [
+                {
+                    'title': title,
+                    'subtitle': subtitle,
+                    'image_url': image_url,
+                    'buttons': [buttons],
+                    'default_action': {
+                        'type': 'web_url',
+                        'url': url,
+                        'messenger_extensions': 'TRUE',
+                        'webview_height_ratio': 'COMPACT',
+                    },
+                },
+            ],
+            'buttons': [],
         },
-      ],
-      'buttons': [],
-    },
-  };
+    };
 };
 
 const media = function ({ media_type, attachment_id }) {
-  return {
-    'attachment': {
-      'type': 'template',
-      'payload': {
-        'template_type': 'media',
-        'elements': [
-          {
-            'media_type': media_type,
-            'attachment_id': attachment_id,
-          },
-        ],
-      },
-    },
-  };
+    return {
+        'attachment': {
+            'type': 'template',
+            'payload': {
+                'template_type': 'media',
+                'elements': [
+                    {
+                        'media_type': media_type,
+                        'attachment_id': attachment_id,
+                    },
+                ],
+            },
+        },
+    };
 };
 
 const generic_web = function ({ title, image_url, subtitle, url, buttons }) {
-  return {
-    "payload": {
-      "template_type": "generic",
-      "elements": [
-        {
-          "title": title,
-          "image_url": image_url,
-          "subtitle": subtitle,
-          "default_action": {
-            "type": "web_url",
-            "url": url,
-            "webview_height_ratio": "tall",
-          },
-          "buttons": [buttons]
+    return {
+        "payload": {
+            "template_type": "generic",
+            "elements": [
+                {
+                    "title": title,
+                    "image_url": image_url,
+                    "subtitle": subtitle,
+                    "default_action": {
+                        "type": "web_url",
+                        "url": url,
+                        "webview_height_ratio": "tall",
+                    },
+                    "buttons": [buttons]
+                }
+            ]
         }
-      ]
     }
-  }
 }
 
 module.exports = {
-  generic,
-  quickReply,
-  button,
-  list,
-  media,
-  generic_web
+    generic,
+    quickReply,
+    button,
+    list,
+    media,
+    generic_web
 };
