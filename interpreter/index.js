7183100fca6f06a84473ef7e08166c4947cdcdf1
Jad Salhani <salhanijad@gmail.com>
2019-04-30 03:27:11 -0700

    i know how to fix this

--- interpreter/index.js
+++ interpreter/index.js
@@ -82,7 +82,7 @@ const runLesson = async (sender_psid, received_message) => {
             case 'free_text':
                 // if (userInput === question.branches[0].answer) {
                 currentLesson.answers.push({ value: userInput, questionId: currentProgress, question: question._id });
-                if (currentProgress === "q000") {
+                if (currentProgress === "q001") {
                     question = await Question.findOne({ id: question.branches[0].next_question });
                 }
                 newProgress = question.branches[0].next_question;
